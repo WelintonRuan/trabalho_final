c8192a9d7f78170aa269fc5f0e53165f973001c6
WelintonRuan <welintonruan916@gmail.com>
2026-05-14 15:32:31 -0300

    adicionado sitema de logar do adm

--- trabalho_final.py
+++ trabalho_final.py
@@ -15,16 +15,76 @@ def criar_conexao():
         print(f"Erro ao conectar: {e}")
         return None
 
-def cadastrarAluno():
-    print("Casdastrar alunos")
 
-    nome = input("Coloque o nome do aluno: ").strip()
+def login():
 
-    if not nome:
-        return print("Nome nao deve ser vazio")
-    
-    try:
-        idade = int(input("Idade do aluno: "))
-        turma = int(input("Turma do aluno: "))
-    except ValueError as e:
-        print("{e}")
+    print("\n========= LOGIN =========")
+
+    usuario = input("Usuário: ")
+    senha = input("Senha: ")
+
+    conn = criar_conexao()
+
+    if conn:
+
+        cursor = conn.cursor()
+
+        try:
+
+            cursor.execute(
+                """
+                SELECT cargo
+                FROM usuarios
+                WHERE login = %s AND senha = %s
+                """,
+                (usuario, senha)
+            )
+
+            resultado = cursor.fetchone()
+
+            if resultado:
+
+                cargo = resultado[0]
+
+                print("\nLogin realizado com sucesso!")
+
+                if cargo == "ADM":
+                    menu_adm()
+
+                elif cargo == "PROF":
+                    ...
+
+                elif cargo == "ALUNO":
+                    ...
+
+            else:
+                print("Usuário ou senha incorretos.")
+
+        except Error as e:
+
+            print(f"Erro: {e}")
+
+        finally:
+
+            cursor.close()
+            conn.close()
+
+
+def menu_adm():
+
+    while True:
+
+        print("\n========= MENU ADM =========")
+        print("1 - Cadastrar professor")
+        print("0 - Sair")
+
+        opcao = input("Escolha: ")
+
+        if opcao == "1":
+            ...
+
+        elif opcao == "0":
+            break
+
+        else:
+            print("Opção inválida.")
